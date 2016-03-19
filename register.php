d1efb337b67c948ef6ff18ebf4f88580489559be
terrabl <aterra@smu.edu>
2016-03-19 15:24:11 -0500

    Added the ability to register
    Just the ability to register, have not yet created the ability to add
    classes to a persons account

---
+++ register.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+<title>Register</title>
+</head>
+<body>
+
+
+<form action="registeruser.php" method="post">
+
+<b>Register a new user</b>
+
+<p>First Name:
+<input type="text" name="first_name" size="30" value="" />
+</p>
+
+<p>Last Name:
+<input type="text" name="last_name" size="30" value="" />
+</p>
+
+<p>Email:
+<input type="text" name="email" size="30" value="" />
+</p>
+
+<p>Password:
+<input type="password" name="password" size="255" value="" />
+</p>
+
+<p>
+<input type="submit" name="submit" value="Send" />
+</p>
+
+</form>
+</body>
+</html>
---
+++ registeruser.php
@@ -0,0 +1,96 @@
+<?php
+session_start();
+
+if(isset($_POST['submit'])){
+
+    $data_missing = array();
+
+    if(empty($_POST['first_name'])){
+
+        // Adds name to array
+        $data_missing[] = 'First Name';
+
+    } else {
+
+        // Trim white space from the name and store the name
+        $f_name = trim($_POST['first_name']);
+
+    }
+
+    if(empty($_POST['last_name'])){
+
+        // Adds name to array
+        $data_missing[] = 'Last Name';
+
+    } else{
+
+        // Trim white space from the name and store the name
+        $l_name = trim($_POST['last_name']);
+
+    }
+
+    if(empty($_POST['email'])){
+
+        // Adds name to array
+        $data_missing[] = 'Email';
+
+    } else {
+
+        // Trim white space from the name and store the name
+        $email = trim($_POST['email']);
+
+    }
+
+    if(empty($_POST['password'])){
+
+        // Adds name to array
+        $data_missing[] = 'Password';
+
+    } else {
+
+        // Trim white space from the name and store the name
+        $reg_password = trim($_POST['password']);
+        $salt = "grupristhebestappever!@#%!^#!$^";
+        $reg_password = crypt($reg_password, $salt);
+
+    }
+
+
+
+    if(empty($data_missing)){
+
+        $hostname = "localhost";
+        $username = "root";
+        $dbname = "grupr";
+        $password = "password";
+        $dbc = mysqli_connect($hostname, $username, $password) OR DIE ("Unable to
+        connect to database! Please try again later.");
+        mysqli_select_db($dbc, $dbname);
+        echo "Connected! <br>";
+        echo "$reg_password <br>";
+
+        $stmt = mysqli_prepare($dbc, "INSERT INTO user_info (email, password, session_id, first_name, last_name)
+        VALUES (?,?,?,?,?)");
+        mysqli_stmt_bind_param($stmt, 'sssss', $email, $reg_password, $_SESSION['session_id'], $f_name, $l_name);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_close($stmt);
+
+        echo 'User Entered';
+
+
+
+    } else {
+
+        echo 'You need to enter the following data<br />';
+
+        foreach($data_missing as $missing){
+
+            echo "$missing<br />";
+
+        }
+
+    }
+
+}
+mysqli_close($dbc);
+?>
