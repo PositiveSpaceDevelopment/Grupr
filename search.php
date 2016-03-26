c7c3352e86ad464d2d48c1276a54b17f5e805036
terrabl <aterra@smu.edu>
2016-03-26 18:03:33 -0500

    ability to search other users
    you can search by first_name and last_name, or only one or the other.

---
+++ search.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+<title>Search</title>
+</head>
+<body>
+
+
+<form action="searchuser.php" method="post">
+
+<b>Search for another user</b>
+
+<p>First Name:
+<input type="text" name="first_name" size="30" value="" />
+</p>
+
+<p>Last Name:
+<input type="text" name="last_name" size="30" value="" />
+</p>
+
+<p>
+<input type="submit" name="submit" value="Send" />
+</p>
+
+</form>
+</body>
+</html>
