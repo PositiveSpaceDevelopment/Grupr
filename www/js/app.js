13ea5fe1ed62bb1f21ea02fae44af28149a9e633
joshthomas735 <Josh@Joshs-MacBook-Pro-2.local>
2016-03-29 20:34:40 -0500

    Tinkering around with the Login and register pages

--- www/js/app.js
+++ www/js/app.js
@@ -37,13 +37,33 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   $stateProvider
 
   // setup an abstract state for the tabs directive
-    .state('tab', {
+  .state('tab', {
     url: '/tab',
     abstract: true,
     templateUrl: 'templates/tabs.html'
   })
 
   // Each tab has its own nav history stack:
+
+  .state('tab.login', {
+    url: '/login',
+    views: {
+      'login': {
+        templateUrl: 'templates/login.html',
+        controller: 'LoginCtrl'
+      }
+    }
+  })
+
+  .state('tab.register', {
+    url: '/register',
+    views: {
+      'register': {
+        templateUrl: 'templates/register.html',
+        controller: 'RegisterCtrl'
+      }
+    }
+  })
 
   .state('tab.browse', {
     url: '/browse',
@@ -95,6 +115,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/browse');
+  $urlRouterProvider.otherwise('/tab/login');
 
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,8 +1,12 @@
 angular.module('starter.controllers', [])
 
-.controller('BrowseCtrl', function($scope) {})
+.controller('BrowseCtrl', function($scope) {
 
-.controller('ViewAllGrupsCtrl', function($scope) {})
+})
+
+.controller('ViewAllGrupsCtrl', function($scope) {
+
+})
 
 .controller('addClassCtrl', function($scope) {
   
@@ -10,6 +14,77 @@ angular.module('starter.controllers', [])
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
   $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('LoginCtrl', function($scope, $state) {
+    $scope.form = {};
+
+  //calls the login function, which puts the data entered in the login 
+  //page and stores it into the 'data' object
+  //This 'data' object is then passed to the POST request
+  $scope.login = function() {
+    var data = {};
+
+    if ($scope.form.email) {
+      data.email = $scope.form.email;
+    };
+    if ($scope.form.password) {
+      data.password = $scope.form.password;
+    };
+    
+    console.log(data);
+
+
+    // //My attempt at getting http POST to work. Not sure how to test this.
+    // // Ill re-visit this when I learn more about how to test it
+    // $http({
+    //   method: 'POST',
+    //   url: '',
+    //   headers: {
+    //     'Content-Type': JSON
+    //     },
+    //   data: data
+    // }).then(function(){
+
+    // });
+
+    // post('', data);
+
+  }
+
+  //Calls the 'register()' function, which is called when the 'Register' button
+  // is clicked on the login page.
+  //It takes the user to the Register page
+  $scope.register = function() {
+    $state.go('tab.register');
+  }
+})
+
+.controller('RegisterCtrl', function($scope. $state) {
+	$scope.form = {};
+
+	//This function is called when the 'next >>' button is clicked at the 
+	// bottom of the Register screen. It performs two functions:
+	// 1) stores the data entered on the screen into the 'data' object
+	// 2) advances the user to the Select Classes screen. This ensures that 
+	//    the user is not able to complete registration without selecting classes
+	$scope.next = function() {
+		var data = {};
+
+		if ($scope.form.name) {
+		  data.name = $scope.form.name;
+		};
+		if ($scope.form.email) {
+		  data.email = $scope.form.email;
+		};
+		if ($scope.form.password) {
+		  data.password = $scope.form.password;
+		};
+
+		console.log(data);
+
+		$state.go('tab.selectClass');
+	}
 })
 
 .controller('ProfileCtrl', function($scope) {
