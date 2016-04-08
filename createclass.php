46a548f6536f0c1e9204a0b4788b51c5b2610035
Bryce Stevenson <bstevenson@smu.edu>
2016-04-07 23:19:14 -0500

    Changed some stuff around

--- createclass.php
+++ createclass.php
@@ -5,9 +5,11 @@
  *  @author Bryce Stevenson
 **/
 <?php
-  $app->post('/createclass', function() {
+  $app->post('/addclass', function() {
     global $conn;
     global $app;
+
+    // $data = json_decode($json);
 
     $userId = $app->request()->post('usrId');
     $courseSubject = $app->request()->post('courseSubj');
@@ -16,7 +18,7 @@
     $classIDNum = $conn->query("SELECT class_id FROM classes WHERE user_id = $userId AND class_subject = $courseSubject AND class_number = $courseNumber");
     $courseExists = $classIDNum->fetch_assoc();
 
-    $result['status'] = 1;
+    // $result['status'] = 1;
     //If the course does not exist add it to the class table.
     if($courseExists == NULL) {
       //The course doesnt exit add it to the table.
@@ -25,9 +27,9 @@
         //Insert data into the bridge table
         $conn->query("INSERT INTO students (user_id, class_id) VALUES ($userId, $classIDNum)");
       }
-      //Error inserting new class
+      //Error inserting new clas
       else {
-        $result['status'] = 2;
+        // $result['status'] = 2;
       }
     }
     //The course does exist. Add the user to the bridge table.
@@ -35,6 +37,7 @@
       $conn->query("INSERT INTO students (user_id, class_id) VALUES ($userId, $classIDNum)");
     }
 
+    //Send back all of the users classes
     echo json_encode($result)
     return;
 });
