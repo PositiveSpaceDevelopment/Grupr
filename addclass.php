c0e810a4433d1ce47562b3c6a23424ecbf647c14
Bryce Stevenson <bstevenson@smu.edu>
2016-04-10 20:55:22 -0500

    /addclass is now all PDO.

--- addclass.php
+++ addclass.php
@@ -5,6 +5,18 @@
  *  @author Bryce Stevenson
 **/
 <?php
+// Routes
+
+// session_start();
+
+$app->get('/[{name}]', function ($request, $response, $args) {
+    // Sample log message
+    $this->logger->info("Slim-Skeleton '/' route");
+
+    // Render index view
+    return $this->renderer->render($response, 'index.phtml', $args);
+});
+
 $app->post('/addclass', function($request, $response, $args) {
 
   $body = $request->getBody();
@@ -15,20 +27,46 @@ $app->post('/addclass', function($request, $response, $args) {
   $courseSubject = $decode->class_subject;
   $courseNumber = $decode->class_number;
 
-  $query = 'SELECT class_id FROM classes WHERE user_id LIKE :userId AND class_subject Like :courseSubject AND class_number LIKE :courseNumber';
+  // $query = 'SELECT class_id FROM classes WHERE user_id LIKE :userId AND class_subject Like :courseSubject AND class_number LIKE :courseNumber';
+  // $stmt = $dbc->prepare($query);
+  // $like = '%';
+  // $userId = $like . $userId . $like;
+  // $courseSubject = $like . $courseSubject . $like;
+  // $courseNumber = $like . $courseNumber . $like;
+  //
+  // $stmt->bindParam(':userId', $userId);
+  // $stmt->bindParam(':courseSubject', $courseSubject);
+  // $stmt->bindParam(':courseNumber', $courseNumber);
+  // $stmt->execute();
+
+  $query = 'SELECT class_id FROM classes WHERE user_id =? AND class_subject =? AND class_number =?;';
   $stmt = $dbc->prepare($query);
-  $like = '%';
-  $userId = $like . $userId . $like;
-  $courseSubject = $like . $courseSubject . $like;
-  $courseNumber = $like . $courseNumber . $like;
+  $stmt->execute([$userId, $courseSubject, $courseNumber]);
+  $classIDNum = $stmt->fetchColumn(0);
 
-  $stmt->bindParam(':userId', $userId);
-  $stmt->bindParam(':courseSubject', $courseSubject);
-  $stmt->bindParam(':courseNumber', $courseNumber);
-  $stmt->execute();
+  //If the course does not exist add it to the class table.
+  if($classIDNum == NULL) {
 
-  $class_info = $stmt->fetchAll(PDO::FETCH_OBJ);
-  echo json_encode($class_info);
+    $classesQuery = 'INSERT INTO classes (class_subject,class_number) VALUES(?,?);';
+    $classesTableInsert = $dbc->prepare($classesQuery);
+    $classesTableInsert->execute([$courseSubject, $courseNumber]);
 
+    $stmt->execute([$userId, $courseSubject, $courseNumber]);
+    $classIDNum = $stmt->fetchColumn(0);
+
+    $studentsQuery = 'INSERT INTO students (user_id, class_id) VALUES (?,?);';
+    $studentTableInsert = $dbc->prepare($studentsQuery);
+    $studentTableInsert->execute([$userId, $classIDNum]);
+  }
+
+
+  //The course does exist. Add the user to the bridge table.
+  else {
+    // $conn->query("INSERT INTO students (user_id, class_id) VALUES ($userId, $classIDNum)");
+    $userQuery = 'INSERT INTO students (user_id, class_id) VALUES (?,?);';
+    $insertUser = $dbc->prepare($userQuery);
+    $insertUser->execute([$userId, $classIDNum]);
+  }
+
+  echo json_encode($classIDNum);
 });
-?>
