39d848f7659fcc2d24f04b0b465f2c3fd79b4199
Bryce Stevenson <bstevenson@smu.edu>
2016-04-12 22:02:42 -0500

    Solved issue where class_id was inserting as 0 to the bridge table.

--- addclass.php
+++ addclass.php
@@ -27,29 +27,11 @@ $app->post('/addclass', function($request, $response, $args) {
   $courseSubject = $decode->class_subject;
   $courseNumber = $decode->class_number;
 
-  // $query = 'SELECT class_id FROM classes WHERE user_id LIKE :userId AND class_subject Like :courseSubject AND class_number LIKE :courseNumber';
-  // $stmt = $dbc->prepare($query);
-  // $like = '%';
-  // $userId = $like . $userId . $like;
-  // $courseSubject = $like . $courseSubject . $like;
-  // $courseNumber = $like . $courseNumber . $like;
-  //
-  // $stmt->bindParam(':userId', $userId);
-  // $stmt->bindParam(':courseSubject', $courseSubject);
-  // $stmt->bindParam(':courseNumber', $courseNumber);
-  // $stmt->execute();
-
-  $query =
-  'SELECT classes.class_id
-  FROM classes
-  INNER JOIN students ON classes.class_id = students.class_id
-  WHERE user_id = ? AND class_subject =? AND class_number = ?';
+  $query = 'SELECT classes.class_id FROM classes WHERE class_subject =? AND class_number = ?';
 
   $stmt = $dbc->prepare($query);
-  $stmt->execute([$userId, $courseSubject, $courseNumber]);
+  $stmt->execute([$courseSubject, $courseNumber]);
   $classIDNum = $stmt->fetchColumn(0);
-
-  echo "classIDNum: " + $classIDNum;
 
   //If the course does not exist add it to the class table.
   if($classIDNum == NULL) {
@@ -58,9 +40,7 @@ $app->post('/addclass', function($request, $response, $args) {
     $classesTableInsert = $dbc->prepare($classesQuery);
     $classesTableInsert->execute([$courseSubject, $courseNumber]);
 
-    // echo "in if\n";
-
-    $stmt->execute([$userId, $courseSubject, $courseNumber]);
+    $stmt->execute([$courseSubject, $courseNumber]);
     $classIDNum = $stmt->fetchColumn(0);
 
     $studentsQuery = 'INSERT INTO students (user_id, class_id) VALUES (?,?);';
@@ -68,14 +48,13 @@ $app->post('/addclass', function($request, $response, $args) {
     $studentTableInsert->execute([$userId, $classIDNum]);
   }
 
-
   //The course does exist. Add the user to the bridge table.
   else {
-    // $conn->query("INSERT INTO students (user_id, class_id) VALUES ($userId, $classIDNum)");
+
     $userQuery = 'INSERT INTO students (user_id, class_id) VALUES (?,?);';
     $insertUser = $dbc->prepare($userQuery);
     $insertUser->execute([$userId, $classIDNum]);
   }
 
-  echo json_encode($classIDNum);
+  // echo json_encode($classIDNum);
 });
